15974f64162c5c9ce3f460ecd384956799229212
Ifthakharul Alam Shuvo <22163545@student.curtin.edu.au>
2025-11-06 00:18:47 +0800

    Fix route to serve static HTML file with student ID

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page - serve static HTML */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'World' });
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 export default router;
